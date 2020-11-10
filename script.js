ff60ef3ef830d513838bd980e021a5358ebc883f
Brien Russell <brussell005@regis.edu>
2020-11-09 18:45:30 -0600

    added loader

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
 const imageContainer = document.getElementById('image-container');
 const loader = document.getElementById('loader');
 
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
 let photosArray = [];
 
 // Accessing Unsplash API
 
 const count = 10;
-const query = 'cute animal';
+const query = 'animal';
 const apiKey = 'ghQQMXVZNIzYarSmCP3HWBQeQcae4fp7qRet28mi5R4';
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}&query=${query}`;
+
+function imageLoaded() {
+    imagesLoaded++;
+    if (imagesLoaded === totalImages) {
+        ready = true;
+        loader.hidden = true;
+    }
+}
 
 function setAttributes(element, attributes) {
     for (const key in attributes) {
@@ -17,6 +28,8 @@ function setAttributes(element, attributes) {
 }
 
 function displayPhotos() {
+    imagesLoaded = 0;
+    totalImages = photosArray.length;
     photosArray.forEach((photo) => {
         // Create <a> to link to Unsplash
         const item = document.createElement('a');
@@ -31,6 +44,7 @@ function displayPhotos() {
             alt: photo.alt_description,
             title: photo.alt_description,
         });
+        img.addEventListener('load', imageLoaded);
         // Put <img> inside <a>, then put both inside imageContainer Element
         item.appendChild(img);
         imageContainer.appendChild(item);
@@ -48,5 +62,14 @@ async function getPhotos() {
     } catch (error) {
     }
 }
+
+window.addEventListener('scroll', () => {
+    if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 && ready) {
+        ready = false;
+        getPhotos();
+
+    }
+
+});
 
 getPhotos();
